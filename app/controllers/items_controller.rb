f3887b2be500be5d9facb4e70bb4805897e54d97
KatsujiOgawa <marug711@gmail.com>
2020-11-09 16:46:51 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,17 @@ before_action :authenticate_user! ,except: [:index]
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :info, :price, :category_id, :condition_id, :shipping_payer_id, :prefecture_id, :shipping_days_id).merge(user_id: current_user.id)
   end
 
 end
