fa2a95ee8e55fb44e3bd0b2bdf72614ac6ea3f23
KatsujiOgawa <marug711@gmail.com>
2020-11-09 09:50:10 +0900

    userモデル単体テスト（異常系）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+         
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'Full-width characters' } do
+    validates :first_name
+    validates :last_name
+  end
 
-  validates :nickname, presence:true
-  validates :first_name, presence:true
-  validates :last_name, presence:true
-  validates :first_name_katakana, presence:true
-  validates :last_name_katakana, presence:true
-  validates :birthday_id, presence:true
+  with_options presence: true, format: { with: /\A[ァ-ン一]+\z/, message: 'Full-width katakana characters' } do
+    validates :first_name_katakana
+    validates :last_name_katakana
+  end
+
+  validates :nickname, presence: true
+  validates :birthday_id, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname                {Faker::Name.initials(number: 2)}
+    email                   {Faker::Internet.free_email}
+    password                {"abc123"}
+    # password                {Faker::Internet.password(min_length: 6)}
+    password_confirmation   {password}
+    first_name              {"勝次"}
+    last_name               {"小川"}
+    first_name_katakana     {"カツジ"}
+    last_name_katakana      {"オガワ"}
+    birthday_id             {"1932-03-03"}
+  end
+end
