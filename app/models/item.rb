02ceb34fc063b309681eab83b6b2fac8b2751bf9
KatsujiOgawa <marug711@gmail.com>
2020-11-10 14:50:36 +0900

    販売手数料、販売利益の非同期実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,17 @@ class Item < ApplicationRecord
   has_one :buy
   has_one_attached :image
 
+  validates :image, presence: true
+  with_options presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }, numericality: { :greater_than_or_equal_to => 300 }, numericality: { :less_than_or_equal_to => 9999999 } do
+    validates :price
+  end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_payer
   belongs_to :prefecture
+  validates :name, presence: true
+  validates :info, presence: true
   validates :category_id, numericality: { other_than: 1 } 
   validates :condition_id, numericality: { other_than: 1 }
   validates :shipping_payer_id, numericality: { other_than: 1 }
