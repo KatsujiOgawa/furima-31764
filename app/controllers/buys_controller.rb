658e998f469044c058e6f1706b1e6a0039b3df5b
KatsujiOgawa <marug711@gmail.com>
2020-11-17 11:16:02 +0900

    フォームオブジェクトによる購入、配送情報の保存機能

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,11 +1,28 @@
 class BuysController < ApplicationController
+
+  
   def index
+    @buy_shipment = BuyShipment.new
   end
 
   def new
   end
 
-  def create
+  def create 
+    @buy_shipment = BuyShipment.new(buy_params)
+    if @buy_shipment.valid?
+      @buy_shipment.save
+      binding.pry
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def buy_params
+    params.require(:buy_shipment).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
+
